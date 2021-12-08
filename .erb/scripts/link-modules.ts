e4e2861bb4a0908ac1e4ee11c33fdb594b5c6c66
hkkim95 <hkkim@seegene.com>
2021-12-08 14:40:45 +0900

    Fix native module install process

--- .erb/scripts/link-modules.ts
+++ .erb/scripts/link-modules.ts
@@ -1,9 +1,23 @@
 import fs from 'fs';
+import path from 'path';
 import webpackPaths from '../configs/webpack.paths';
 
 const srcNodeModulesPath = webpackPaths.srcNodeModulesPath;
-const appNodeModulesPath = webpackPaths.appNodeModulesPath
+const appNodeModulesPath = webpackPaths.appNodeModulesPath;
 
-if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
-  fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
+fs.readdirSync(appNodeModulesPath).forEach(nativeModuleDependency => {
+  const srcCheckPath = path.join(srcNodeModulesPath, nativeModuleDependency)
+  const appCheckPath = path.join(appNodeModulesPath, nativeModuleDependency)
+
+  if (checkModuleValid(srcCheckPath, appCheckPath)) {
+    fs.symlinkSync(appCheckPath, srcCheckPath, 'junction')
+  }
+});
+
+function checkModuleValid(srcCheckPath: any, appCheckPath: any) {
+  return !fs.existsSync(srcCheckPath) && fs.existsSync(appCheckPath) && isDirectory(appCheckPath)
+}
+
+function isDirectory(path:string) {
+  return fs.statSync(path).isDirectory()
 }
